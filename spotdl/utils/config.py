620218e062fc70e9e1412bf12ed83e6375aedb18
Kaio Siqueira <ikaiodev@gmail.com>
2026-05-20 20:43:47 -0300

    fix: make max paramenter zeroed value coherent

--- spotdl/utils/config.py
+++ spotdl/utils/config.py
@@ -368,7 +368,7 @@ DOWNLOADER_OPTIONS: DownloaderOptions = {
     "create_skip_file": False,
     "respect_skip_file": False,
     "sync_remove_lrc": False,
-    "max_songs": 100,
+    "max_songs": 1000,
 }
 
 WEB_OPTIONS: WebOptions = {
